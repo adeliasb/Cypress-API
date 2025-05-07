c5d6122d11b00fc6012f90c22f0bbfc175ccf53e
adeliasb <qa.asbs.lab@gmail.com>
2025-05-06 23:07:13 -0300

    Atualização do arquivobuscar-dispositivo adicionando cy.writeFile

--- cypress/e2e/buscar_dispositivo.cy.js
+++ cypress/e2e/buscar_dispositivo.cy.js
@@ -21,23 +21,23 @@ describe("Buscar dispositivos", () => {
     }
 
     )
-    it("Buscar dispositivo inexistente", () => {
-        cy.request({
-            method: "GET",
-            url: "https://api.restful-api.dev/objects/ERRO", 
-            failOnStatusCode: false           
-        }).then((response) => {
-            console.log("Resposta: " ,response)
-            expect(response.status).to.equal(404)
-            expect(response.body.error).to.equal("Oject with id=ERRO was not found.")
+    // it("Buscar dispositivo inexistente", () => {
+    //     cy.request({
+    //         method: "GET",
+    //         url: "https://api.restful-api.dev/objects/ERRO", 
+    //         failOnStatusCode: false           
+    //     }).then((response) => {
+    //         console.log("Resposta: " ,response)
+    //         expect(response.status).to.equal(404)
+    //         expect(response.body.error).to.equal("Oject with id=ERRO was not found.")
 
-        }
+    //     }
 
-        )
+    //     )
 
-    }
+    // }
 
-    )
+    // )
     // it("Buscar dispositivo por ID", () => {
     //     cy.request({
     //         method: "GET",
